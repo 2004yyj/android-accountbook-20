2f6540b81d2d34883329d3cc79931574dd57b0e4
2004yyj <2004yyj@naver.com>
2022-07-29 00:07:00 +0900

    [FIX] Result GetValue

--- domain/src/main/java/com/woowahan/accountbook/domain/model/Result.kt
+++ domain/src/main/java/com/woowahan/accountbook/domain/model/Result.kt
@@ -3,7 +3,7 @@ package com.woowahan.accountbook.domain.model
 import java.lang.Exception
 
 sealed class Result<out T> {
-    class Success<T>(value: T): Result<T>()
-    class Failure(cause: Exception): Result<Nothing>()
+    class Success<T>(val value: T): Result<T>()
+    class Failure(val cause: Exception): Result<Nothing>()
     object Loading: Result<Nothing>()
 }
