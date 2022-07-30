a6a8321f6ceee787ec2542735f088715325a6152
2004yyj <2004yyj@naver.com>
2022-07-30 17:51:36 +0900

    [DELETE] Unnecessary Code

--- presentation/src/main/java/com/woowahan/accountbook/components/calendar/CalendarScreen.kt
+++ presentation/src/main/java/com/woowahan/accountbook/components/calendar/CalendarScreen.kt
@@ -1,11 +1,7 @@
 package com.woowahan.accountbook.components.calendar
 
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import com.woowahan.accountbook.ui.theme.Icons
 
 @Composable
 fun CalendarScreen() {
-    val name = object{}.javaClass.enclosingClass.name
-    Text("Hello $name")
 }
