d21e87ba113f99fd7d71b3d9333edd7497219e30
2004yyj <2004yyj@naver.com>
2022-07-31 20:29:58 +0900

    [FIX] calendarState의 value가 변할 때마다 계산 시행

--- presentation/src/main/java/com/woowahan/accountbook/components/calendar/datelist/AccountBookCalendar.kt
+++ presentation/src/main/java/com/woowahan/accountbook/components/calendar/datelist/AccountBookCalendar.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,27 +31,30 @@ fun AccountBookCalendar(
     dividerColor: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.12f),
     content: @Composable() (BoxScope.(date: Int, isCurrentMonth: Boolean) -> Unit),
 ) {
-    val calendarDates = ArrayList<CalendarDate>()
+    val calendarDates = remember { mutableStateListOf<CalendarDate>() }
 
-    calendarState.run {
-        val prevLastDate = prevLastDay.get(Calendar.DATE)
-        val prevLastDayOfWeek = prevLastDay.get(Calendar.DAY_OF_WEEK)
+    LaunchedEffect(key1 = calendarState.value) {
+        calendarState.run {
+            calendarDates.clear()
+            val prevLastDate = prevLastDay.get(Calendar.DATE)
+            val prevLastDayOfWeek = prevLastDay.get(Calendar.DAY_OF_WEEK)
 
-        val curLastDate = curLastDay.get(Calendar.DATE)
-        val curLastDayOfWeek = curLastDay.get(Calendar.DAY_OF_WEEK)
+            val curLastDate = curLastDay.get(Calendar.DATE)
+            val curLastDayOfWeek = curLastDay.get(Calendar.DAY_OF_WEEK)
 
-        if (prevLastDayOfWeek != 7) {
-            for (i in prevLastDayOfWeek - 1 downTo 0) {
-                calendarDates.add(CalendarDate(prevLastDate-i, false))
+            if (prevLastDayOfWeek != 7) {
+                for (i in prevLastDayOfWeek - 1 downTo 0) {
+                    calendarDates.add(CalendarDate(prevLastDate - i, false))
+                }
             }
-        }
 
-        for (i in 1..curLastDate) {
-            calendarDates.add(CalendarDate(i, true))
-        }
+            for (i in 1..curLastDate) {
+                calendarDates.add(CalendarDate(i, true))
+            }
 
-        for (i in 1..(7 - curLastDayOfWeek)) {
-            calendarDates.add(CalendarDate(i, false))
+            for (i in 1..(7 - curLastDayOfWeek)) {
+                calendarDates.add(CalendarDate(i, false))
+            }
         }
     }
 
