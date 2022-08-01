201ecd8c75b870a82fe1f0d20e60e672cd211a3b
2004yyj <2004yyj@naver.com>
2022-08-01 18:52:38 +0900

    [ADD] 값이 없을 때 Empty View 보여주기

--- presentation/src/main/java/com/woowahan/accountbook/components/calendar/CalendarScreen.kt
+++ presentation/src/main/java/com/woowahan/accountbook/components/calendar/CalendarScreen.kt
@@ -63,143 +63,156 @@ fun CalendarScreen(
                 .fillMaxSize()
                 .padding(it)
         ) {
-            AccountBookCalendar(
-                modifier = Modifier.fillMaxSize(),
-                calendarState = calendarState,
-                dividerColor = PurpleLight40,
-                footer = {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 24.dp)
-                    ) {
-                        Text(
-                            text = "수입",
-                            color = Purple,
-                            style = Typography.subtitle1,
-                            modifier = Modifier.align(Alignment.CenterStart)
-                        )
-
-                        Text(
-                            text = incomeTotal.toMoneyString(),
-                            color = Success,
-                            fontWeight = FontWeight.Bold,
-                            style = Typography.subtitle1,
-                            modifier = Modifier.align(Alignment.CenterEnd)
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    Divider(
-                        modifier = Modifier
-                            .height(1.dp)
-                            .fillMaxWidth(),
-                        color = PurpleLight40
-                    )
-
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 10.dp)
-                    ) {
-                        Text(
-                            text = "지출",
-                            color = Purple,
-                            style = Typography.subtitle1,
-                            modifier = Modifier.align(Alignment.CenterStart)
-                        )
-
-                        Text(
-                            text = (expenseTotal * -1).toMoneyString(),
-                            color = Error,
-                            fontWeight = FontWeight.Bold,
-                            style = Typography.subtitle1,
-                            modifier = Modifier.align(Alignment.CenterEnd)
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    Divider(
-                        modifier = Modifier
-                            .height(1.dp)
-                            .fillMaxWidth(),
-                        color = PurpleLight40
-                    )
-
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 10.dp)
-                    ) {
-                        Text(
-                            text = "총합",
-                            color = Purple,
-                            style = Typography.subtitle1,
-                            modifier = Modifier.align(Alignment.CenterStart)
-                        )
-
-                        Text(
-                            text = (incomeTotal - expenseTotal).toMoneyString(),
-                            color = Purple,
-                            fontWeight = FontWeight.Bold,
-                            style = Typography.subtitle1,
-                            modifier = Modifier.align(Alignment.CenterEnd)
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(10.dp))
-                }
-            ) { date, isCurrentMonth ->
-                Column(
-                    modifier = Modifier
+            if (incomeTotal == 0L && expenseTotal == 0L) {
+                Box(
+                    Modifier
                         .fillMaxSize()
-                        .align(Alignment.Center)
+                        .padding(horizontal = 16.dp, vertical = 9.dp)
                 ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = "내역이 없습니다."
+                    )
+                }
+            } else {
+                AccountBookCalendar(
+                    modifier = Modifier.fillMaxSize(),
+                    calendarState = calendarState,
+                    dividerColor = PurpleLight40,
+                    footer = {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 24.dp)
+                        ) {
+                            Text(
+                                text = "수입",
+                                color = Purple,
+                                style = Typography.subtitle1,
+                                modifier = Modifier.align(Alignment.CenterStart)
+                            )
+
+                            Text(
+                                text = incomeTotal.toMoneyString(),
+                                color = Success,
+                                fontWeight = FontWeight.Bold,
+                                style = Typography.subtitle1,
+                                modifier = Modifier.align(Alignment.CenterEnd)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Divider(
+                            modifier = Modifier
+                                .height(1.dp)
+                                .fillMaxWidth(),
+                            color = PurpleLight40
+                        )
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 10.dp)
+                        ) {
+                            Text(
+                                text = "지출",
+                                color = Purple,
+                                style = Typography.subtitle1,
+                                modifier = Modifier.align(Alignment.CenterStart)
+                            )
+
+                            Text(
+                                text = (expenseTotal * -1).toMoneyString(),
+                                color = Error,
+                                fontWeight = FontWeight.Bold,
+                                style = Typography.subtitle1,
+                                modifier = Modifier.align(Alignment.CenterEnd)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Divider(
+                            modifier = Modifier
+                                .height(1.dp)
+                                .fillMaxWidth(),
+                            color = PurpleLight40
+                        )
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 10.dp)
+                        ) {
+                            Text(
+                                text = "총합",
+                                color = Purple,
+                                style = Typography.subtitle1,
+                                modifier = Modifier.align(Alignment.CenterStart)
+                            )
+
+                            Text(
+                                text = (incomeTotal - expenseTotal).toMoneyString(),
+                                color = Purple,
+                                fontWeight = FontWeight.Bold,
+                                style = Typography.subtitle1,
+                                modifier = Modifier.align(Alignment.CenterEnd)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(10.dp))
+                    }
+                ) { date, isCurrentMonth ->
                     Column(
                         modifier = Modifier
-                            .weight(2f)
-                            .align(Alignment.Start)
+                            .fillMaxSize()
+                            .align(Alignment.Center)
                     ) {
-                        if (isCurrentMonth) {
-                            val filter = history.filter { it.date == currentMonth.getCurrentDateMillis(date) }
-                            if (filter.isNotEmpty()) {
-                                var income = 0L
-                                var expense = 0L
-                                filter.forEach {
-                                    if (it.amount > 0) income += it.amount
-                                    else expense += it.amount
+                        Column(
+                            modifier = Modifier
+                                .weight(2f)
+                                .align(Alignment.Start)
+                        ) {
+                            if (isCurrentMonth) {
+                                val filter = history.filter { it.date == currentMonth.getCurrentDateMillis(date) }
+                                if (filter.isNotEmpty()) {
+                                    var income = 0L
+                                    var expense = 0L
+                                    filter.forEach {
+                                        if (it.amount > 0) income += it.amount
+                                        else expense += it.amount
+                                    }
+                                    if (income != 0L)
+                                        Text(
+                                            text = income.toMoneyString(),
+                                            fontSize = 10.sp,
+                                            color = Success,
+                                            maxLines = 1,
+                                            fontWeight = FontWeight.Bold
+                                        )
+                                    if (expense != 0L)
+                                        Text(
+                                            text = (expense * -1).toMoneyString(),
+                                            fontSize = 10.sp,
+                                            color = Error,
+                                            maxLines = 1,
+                                            fontWeight = FontWeight.Bold
+                                        )
                                 }
-                                if (income != 0L)
-                                    Text(
-                                        text = income.toMoneyString(),
-                                        fontSize = 10.sp,
-                                        color = Success,
-                                        maxLines = 1,
-                                        fontWeight = FontWeight.Bold
-                                    )
-                                if (expense != 0L)
-                                    Text(
-                                        text = (expense * -1).toMoneyString(),
-                                        fontSize = 10.sp,
-                                        color = Error,
-                                        maxLines = 1,
-                                        fontWeight = FontWeight.Bold
-                                    )
                             }
                         }
+                        Spacer(modifier = Modifier.height(24.dp))
+                        Text(
+                            text = date.toString(),
+                            fontSize = 10.sp,
+                            modifier = Modifier
+                                .weight(1f)
+                                .align(Alignment.End),
+                            fontWeight = FontWeight.Bold,
+                            color = if (isCurrentMonth) Purple else PurpleLight40
+                        )
                     }
-                    Spacer(modifier = Modifier.height(24.dp))
-                    Text(
-                        text = date.toString(),
-                        fontSize = 10.sp,
-                        modifier = Modifier
-                            .weight(1f)
-                            .align(Alignment.End),
-                        fontWeight = FontWeight.Bold,
-                        color = if (isCurrentMonth) Purple else PurpleLight40
-                    )
                 }
             }
         }
