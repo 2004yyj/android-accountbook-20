de7fbe1f5c3f5a1492e4f49b604f588dfefa080d
2004yyj <2004yyj@naver.com>
2022-08-02 19:46:58 +0900

    [FIX] 수정 및 추가 화면으로 넘어갈 때 기존 아이템 id를 넘기기

--- presentation/src/main/java/com/woowahan/accountbook/components/setting/create/PaymentMethodCreateScreen.kt
+++ presentation/src/main/java/com/woowahan/accountbook/components/setting/create/PaymentMethodCreateScreen.kt
@@ -1,8 +1,12 @@
 package com.woowahan.accountbook.components.setting.create
 
 import androidx.compose.runtime.Composable
+import com.woowahan.accountbook.domain.model.PaymentMethod
 
 @Composable
-fun PaymentMethodCreateScreen(settingMode: String) {
+fun PaymentMethodCreateScreen(
+    settingMode: String,
+    id: Int,
+) {
 
 }
