19137dcf7bfc473822b807ea1a189eaa378a8824
2004yyj <2004yyj@naver.com>
2022-08-02 18:09:53 +0900

    [FIX] Category Type String 에서 Enum으로 변경

--- data/src/main/java/com/woowahan/accountbook/data/mapper/CategoryMapper.kt
+++ data/src/main/java/com/woowahan/accountbook/data/mapper/CategoryMapper.kt
@@ -2,15 +2,16 @@ package com.woowahan.accountbook.data.mapper
 
 import com.woowahan.accountbook.data.entity.CategoryData
 import com.woowahan.accountbook.domain.model.Category
+import com.woowahan.accountbook.domain.model.PaymentType
 
 fun CategoryData.toModel(): Category {
     return Category(
-        id, type, name, color
+        id, PaymentType.valueOf(type), name, color
     )
 }
 
 fun Category.toEntity(): CategoryData {
     return CategoryData(
-        id, type, name, color
+        id, type.toString(), name, color
     )
 }
