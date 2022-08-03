9eb8dc1fbd30301926581db2c8ca2df7d0ae7f98
2004yyj <2004yyj@naver.com>
2022-08-03 20:40:45 +0900

    [ADD] BackAppBar

--- presentation/src/main/java/com/woowahan/accountbook/ui/screens/statistics/detail/StatisticsDetailScreen.kt
+++ presentation/src/main/java/com/woowahan/accountbook/ui/screens/statistics/detail/StatisticsDetailScreen.kt
@@ -1,16 +1,22 @@
 package com.woowahan.accountbook.ui.screens.statistics.detail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.woowahan.accountbook.ui.components.appbar.BackAppBar
 import com.woowahan.accountbook.ui.components.graph.LineGraph
+import com.woowahan.accountbook.ui.theme.White
 import com.woowahan.accountbook.ui.viewmodel.statistics.detail.StatisticsDetailViewModel
 
 @Composable
@@ -24,8 +30,24 @@ fun StatisticsDetailScreen(
         viewModel.getTotalList(categoryName)
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        LineGraph(entries = entries)
-        Text(text = "asdfasf")
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            BackAppBar(
+                title = { Text(text = categoryName) },
+                onClickBack = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            LineGraph(
+                modifier = Modifier
+                    .background(White)
+                    .padding(bottom = 5.dp),
+                entries = entries
+            )
+        }
     }
 }
