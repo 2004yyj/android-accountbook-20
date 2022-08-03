2dc87a921b9ffc1d5361190f8201c63b9e254242
2004yyj <2004yyj@naver.com>
2022-08-03 12:56:28 +0900

    [ADD] MainActivity에 StatisticsDetailScreen 화면 연동

---
+++ presentation/src/main/java/com/woowahan/accountbook/ui/screens/statistics/detail/StatisticsDetailScreen.kt
@@ -0,0 +1,12 @@
+package com.woowahan.accountbook.ui.screens.statistics.detail
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun StatisticsDetailScreen(
+    categoryName: String,
+    navController: NavHostController
+) {
+
+}
