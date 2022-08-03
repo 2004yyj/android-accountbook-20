84b8c144ab0565127e95e387f759400fdfa5993d
2004yyj <2004yyj@naver.com>
2022-08-03 21:16:05 +0900

    [FIX]

--- presentation/src/main/java/com/woowahan/accountbook/di/module/usecase/HistoryUseCaseModule.kt
+++ presentation/src/main/java/com/woowahan/accountbook/di/module/usecase/HistoryUseCaseModule.kt
@@ -69,7 +69,8 @@ object HistoryUseCaseModule {
     @Singleton
     fun providesGetAllExpendHistoriesByMonthAndCategoryNameUseCase(repository: HistoryRepository): GetAllExpendHistoriesByMonthAndCategoryNameUseCase {
         return GetAllExpendHistoriesByMonthAndCategoryNameUseCase(repository)
-    
+    }
+
     @Provides
     @Singleton
     fun providesGetHistoryByIdUseCase(repository: HistoryRepository): GetHistoryByIdUseCase {
