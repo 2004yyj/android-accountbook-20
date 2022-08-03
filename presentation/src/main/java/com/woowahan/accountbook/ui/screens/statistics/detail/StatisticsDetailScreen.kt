3a09ebe2323e9440eb87aceabcb8d0e9117692de
2004yyj <2004yyj@naver.com>
2022-08-03 20:33:00 +0900

    [ADD] 달별 Total 가져오는 메소드 연동

--- presentation/src/main/java/com/woowahan/accountbook/ui/screens/statistics/detail/StatisticsDetailScreen.kt
+++ presentation/src/main/java/com/woowahan/accountbook/ui/screens/statistics/detail/StatisticsDetailScreen.kt
@@ -1,12 +1,31 @@
 package com.woowahan.accountbook.ui.screens.statistics.detail
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.woowahan.accountbook.ui.components.graph.LineGraph
+import com.woowahan.accountbook.ui.viewmodel.statistics.detail.StatisticsDetailViewModel
 
 @Composable
 fun StatisticsDetailScreen(
     categoryName: String,
-    navController: NavHostController
+    navController: NavHostController,
+    viewModel: StatisticsDetailViewModel = hiltViewModel()
 ) {
+    val entries by viewModel.totalEntries.collectAsState()
+    LaunchedEffect(key1 = entries.isEmpty()) {
+        viewModel.getTotalList(categoryName)
+    }
 
+    Column(modifier = Modifier.fillMaxSize()) {
+        LineGraph(entries = entries)
+        Text(text = "asdfasf")
+    }
 }
---
+++ presentation/src/main/java/com/woowahan/accountbook/ui/viewmodel/statistics/detail/StatisticsDetailViewModel.kt
@@ -0,0 +1,41 @@
+package com.woowahan.accountbook.ui.viewmodel.statistics.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.woowahan.accountbook.domain.model.Result
+import com.woowahan.accountbook.domain.model.Total
+import com.woowahan.accountbook.domain.usecase.history.GetTotalListByCategoryNameGroupByDateUseCase
+import com.woowahan.accountbook.ui.components.graph.entry.line.LineEntry
+import com.woowahan.accountbook.util.toMonth
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import java.util.*
+import javax.inject.Inject
+
+@HiltViewModel
+class StatisticsDetailViewModel @Inject constructor(
+    private val getTotalListByCategoryNameGroupByDateUseCase: GetTotalListByCategoryNameGroupByDateUseCase
+): ViewModel() {
+
+    private val _totalEntries = MutableStateFlow<List<LineEntry>>(emptyList())
+    val totalEntries = _totalEntries.asStateFlow()
+
+    fun getTotalList(name: String) {
+        viewModelScope.launch {
+            getTotalListByCategoryNameGroupByDateUseCase(name).collect {
+                when(it) {
+                    is Result.Success<List<Total>> -> {
+                        val calendar = Calendar.getInstance()
+                        _totalEntries.emit(it.value.map {
+                            LineEntry(it.total * -1, it.date.toMonth())
+                        })
+                    }
+                }
+            }
+        }
+
+    }
+}
