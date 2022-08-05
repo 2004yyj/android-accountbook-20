20bc8ac884f8521de884291ac8892f64a86f297f
2004yyj <2004yyj@naver.com>
2022-08-05 18:35:00 +0900

    [ADD] DatabaseOpenHelper Companion CREATE DROP DDL SQL Queries

--- local/src/main/java/com/woowahan/accountbook/local/helper/DatabaseOpenHelper.kt
+++ local/src/main/java/com/woowahan/accountbook/local/helper/DatabaseOpenHelper.kt
@@ -27,4 +27,12 @@ class DatabaseOpenHelper(
 
 private interface OnUpgradeListener {
     fun onUpgrade(oldVersion: Int, newVersion: Int)
+    companion object {
+        private const val CREATE_CATEGORY = "CREATE TABLE IF NOT EXISTS Category (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT NOT NULL, name TEXT NOT NULL UNIQUE, color TEXT NOT NULL);"
+        private const val DROP_CATEGORY = "DROP TABLE IF EXISTS Category;"
+        private const val CREATE_HISTORY = "CREATE TABLE IF NOT EXISTS History (id INTEGER PRIMARY KEY AUTOINCREMENT, date INTEGER NOT NULL, amount INTEGER NOT NULL, content TEXT NOT NULL, category_id INTEGER REFERENCES Category(id), payment_method_id INTEGER REFERENCES PaymentMethod(id));"
+        private const val DROP_HISTORY = "DROP TABLE IF EXISTS History;"
+        private const val CREATE_PAYMENT_METHOD = "CREATE TABLE IF NOT EXISTS PaymentMethod (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL UNIQUE);"
+        private const val DROP_PAYMENT_METHOD = "DROP TABLE IF EXISTS PaymentMethod"
+    }
 }
